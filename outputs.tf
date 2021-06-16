307ab1d93c57b6e8e9b1e897ded4eceb74c4df27
Tony Vattathil <avattathil@gmail.com>
2021-06-16 16:16:23 -0700

    Add Output "aws_codecommit_repo_arn

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
 
+
+
+
 output "aws_codecommit_repo_name" {
   value = aws_codecommit_repository.cc_repo.repository_id
+}
+output "aws_codecommit_repo_arn" {
+  value = aws_codecommit_repository.cc_repo.arn
 }
